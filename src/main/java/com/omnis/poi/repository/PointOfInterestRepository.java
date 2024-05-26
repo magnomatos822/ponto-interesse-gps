c7af1606cf4c04496a9dbf6eb4a4694e191a2d86
Magno Matos <magnomatos822@gmail.com>
2024-05-25 22:31:25 -0300

    feat: Add repository interface PointOfInteresty

---
+++ src/main/java/com/omnis/poi/repository/PointOfInterestRepository.java
@@ -0,0 +1,7 @@
+package com.omnis.poi.repository;
+
+import com.omnis.poi.entity.PointOfInterest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PointOfInterestRepository extends JpaRepository<PointOfInterest, Long>{
+}

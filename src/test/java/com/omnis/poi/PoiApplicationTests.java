1366f45b454777ca2cd79a7bb5fc317ed1942455
Magno Matos <magnomatos822@gmail.com>
2024-05-25 21:54:32 -0300

    feat: First commit

---
+++ src/test/java/com/omnis/poi/PoiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omnis.poi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
